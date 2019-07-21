aed84f2fc8d1a9bb286340657d06f52bc3ecdc06
Mike OShea <oshea00@gmail.com>
2019-07-21 12:00:42 -0700

    Create Developer type

--- App.Test/AppTests.cs
+++ App.Test/AppTests.cs
@@ -8,9 +8,9 @@ namespace Tests
     {
         List<DeveloperReport> devReports = new List<DeveloperReport>
             {
-                new DeveloperReport {Id = 1, Name = "Dev1", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 160 },
-                new DeveloperReport {Id = 2, Name = "Dev2", Level = "Junior developer", HourlyRate  = 20, WorkingHours = 150 },
-                new DeveloperReport {Id = 3, Name = "Dev3", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 180 }
+                new DeveloperReport {Developer = new Developer{Id = 1, Name = "Dev1", Level = "Senior developer" }, HourlyRate  = 30.5, WorkingHours = 160 },
+                new DeveloperReport {Developer = new Developer{Id = 2, Name = "Dev2", Level = "Junior developer" }, HourlyRate  = 20, WorkingHours = 150 },
+                new DeveloperReport {Developer = new Developer{Id = 3, Name = "Dev3", Level = "Senior developer" }, HourlyRate  = 30.5, WorkingHours = 180 }
             };
 
 
@@ -20,5 +20,6 @@ namespace Tests
             var payroll = new SalaryCalculator(devReports);
             Assert.AreEqual(15444d, payroll.CalculateTotalSalaries());
         }
+
     }
 }
--- App/Program.cs
+++ App/Program.cs
@@ -9,21 +9,30 @@ namespace App
         {
             var devReports = new List<DeveloperReport>
             {
-                new DeveloperReport {Id = 1, Name = "Dev1", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 160 },
-                new DeveloperReport {Id = 2, Name = "Dev2", Level = "Junior developer", HourlyRate  = 20, WorkingHours = 150 },
-                new DeveloperReport {Id = 3, Name = "Dev3", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 180 }
+                new DeveloperReport { Developer = new Developer { Id = 1, Name = "Dev1", Level = "Senior developer" }, HourlyRate  = 30.5, WorkingHours = 160 },
+                new DeveloperReport { Developer = new Developer { Id = 2, Name = "Dev2", Level = "Junior developer" }, HourlyRate  = 20, WorkingHours = 150 },
+                new DeveloperReport { Developer = new Developer { Id = 3, Name = "Dev3", Level = "Senior developer" }, HourlyRate  = 30.5, WorkingHours = 180 }
             };
 
+            var d = new Developer
+            {
+                Id = 1
+            };
             var calculator = new SalaryCalculator(devReports);
             Console.WriteLine($"Sum of all the developer salaries is {calculator.CalculateTotalSalaries()} dollars");
         }
     }
 
-    public class DeveloperReport
+    public class Developer
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Level { get; set; }
+    }
+
+    public class DeveloperReport
+    {
+        public Developer Developer { get; set; }
         public int WorkingHours { get; set; }
         public double HourlyRate { get; set; }
     }
@@ -43,7 +52,7 @@ namespace App
 
             foreach (var devReport in _developerReports)
             {
-                if (devReport.Level == "Senior developer")
+                if (devReport.Developer.Level == "Senior developer")
                 {
                     totalSalaries += devReport.HourlyRate * devReport.WorkingHours * 1.2;
                 }
